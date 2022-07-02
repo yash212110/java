10325365948d5eb7caa8b73abb60b5c29897a2c7
Yash Aghane <104718459+yash212110@users.noreply.github.com>
2022-07-02 07:34:22 -0700

    Update input.java

--- input.java
+++ input.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class funar {
     public static void main(String[] args) {
-       
+       //FS21IF035  YASH AGHANE
         Scanner scn = new Scanner(System.in);
  
         String str1 = scn.next();
