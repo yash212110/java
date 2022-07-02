d297015b71b63fc34d39e1f41da6bd6187f6f2d5
Yash Aghane <104718459+yash212110@users.noreply.github.com>
2022-07-02 07:33:07 -0700

    Add files via upload

---
+++ funar.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class funar {
+    public static void main(String[] args) {
+       
+        Scanner scn = new Scanner(System.in);
+ 
+        String str1 = scn.next();
+        System.out.println("Entered String str1 :  " +str1);
+        
+}
+}
