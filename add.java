c872970c890a66378944012ab868fab441440378
Yash Aghane <104718459+yash212110@users.noreply.github.com>
2022-07-02 21:30:10 -0700

    Add files via upload

---
+++ add.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+ public class add {
+
+    public static void main( String args[] ){
+        // FS21IF035 yash aghane
+    System.out.println("enter first number \n");
+    Scanner sc =new Scanner(System.in);
+
+    int a =sc.nextInt(); //input of first number
+    System.out.println("enter second number");
+
+    int b =sc.nextInt();//input of second number
+    int c=a+b;//operation
+    System.out.println("addition is "+c);
+
+    
+       
+    }
+ }
+
