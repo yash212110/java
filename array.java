132f2fa1773850eee549d1c16e6a61fd200737bd
Yash Aghane <104718459+yash212110@users.noreply.github.com>
2022-07-04 00:18:36 -0700

    Add files via upload

---
+++ array.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+public class array
+{
+    public static void main(String arg[])     //yash aghane 
+    {                                
+        int[] marks = new int[4];   //declare array
+        marks[0]=32;
+        marks[1]=52;
+        marks[2]=58;
+        marks[3]=65;
+
+        for(int i=0;i<4;i++)
+        {
+             System.out.println(marks[i]);     //print with for loop
+        }
+
+    }
+}
