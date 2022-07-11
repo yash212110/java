3a14333cb6a680c347c3e3da89d9b1903b321e2a
Yash Aghane <104718459+yash212110@users.noreply.github.com>
2022-07-10 23:32:49 -0700

    Add files via upload

---
+++ hotyy.java
@@ -0,0 +1,30 @@
+import java.util.scanner;
+/*class student    //define a class
+{
+    public
+    String name;
+    String sirname;  ///yash aghane murbad 421-401
+    
+   public static void play()
+   {                                 //function declration
+    System.out.println("student iss playing");
+   }
+}/* */
+//import java.util.scanner;
+public  class classy{
+
+     
+public static void main(String args[])   //main function
+{    
+      student.play();            //calling
+ 
+      System.out.println("enter fist number");
+      Scanner num=new Scanner(System.in);
+      int num=num.nextINT();
+      System.out.print(num);
+      
+    
+}
+}
+
+  
